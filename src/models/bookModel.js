1d42c90347685d03bcfa747647b35fbf7c56aaff
omsemwal <omsemwal111@gmail.com>
2022-06-07 21:10:38 +0530

    mongoos session2

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
+const BookSchema = new mongoose.Schema({
+
+    BookName:String,
+
+    authorName:String,
+
+   price : {
+    indianPrice: String,
+    europePrice: String
+},
+
+   totalpages: Number,
+   stockAvailable:Boolean,
+
     
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
+   year : {
+        type : Number,
+        default : 2021
     },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+    tags : [String], 
 
-
-module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
+},   {timestamps:true}) 
+module.exports =mongoose.model('Book',BookSchema)
